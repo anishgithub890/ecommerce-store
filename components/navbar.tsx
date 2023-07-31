768758c79c129ce98f1e05f6e9e49cc6768a9a69
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-07-31 19:58:24 +0545

    container

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,14 @@
+import Container from '@/components/ui/container';
+import Link from 'next/link';
+
 const Navbar = () => {
   return (
     <div className="border-b">
-      <div>Navbar!</div>
+      <Container>
+        <Link href="/">
+          <p>STORE</p>
+        </Link>
+      </Container>
     </div>
   );
 };
