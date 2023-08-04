2f1628a147646f5abf3d24970cd03a92a65f5431
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-08-04 14:32:05 +0545

    get-billboard-and-billboard.tsx

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,18 @@
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import getBillboard from '@/actions/get-billboard';
+import Billboard from '@/components/billboard';
+import Container from '@/components/ui/container';
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard('3e615ecf-11d3-4c1c-85c5-6c0eb7ff3e22');
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -2,8 +2,19 @@
 
 import { ShoppingBag } from 'lucide-react';
 import Button from '@/components/ui/button';
+import { useEffect, useState } from 'react';
 
 const NavbarActions = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div className="ml-auto flex items-center gap-x-4">
       <Button className="flex items-center rounded-full px-4 py-2 bg-black">
