ef5164778ae6362a9b57ebca217c814d196d47da
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-08-07 14:31:01 +0545

    info.tsx

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -3,6 +3,9 @@ import getProducts from '@/actions/get-products';
 import ProductList from '@/components/product-list';
 import Container from '@/components/ui/container';
 import Gallery from '@/components/gallery';
+import Info from '@/components/Info';
+
+export const revalidate = 0;
 
 interface ProductPageProps {
   params: {
@@ -23,8 +26,8 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
             {/* Gallery */}
             <Gallery images={product.images} />
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-              {/* Into */}
-              Info
+              {/* Info */}
+              <Info data={product} />
             </div>
           </div>
           <hr className="my-10" />
